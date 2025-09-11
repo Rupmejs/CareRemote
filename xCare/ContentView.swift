22cd3bc437b39ac6b8177a62788a0c1a398e1bb7
Reidzansk <kristapsreidzans@inbox.lv>
2025-09-12 00:16:56 +0300

    asdasda

--- xCare/ContentView.swift
+++ xCare/ContentView.swift
@@ -3,8 +3,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            NavigationLink("Go to SecondView", destination: SecondView())
-                .padding()
+            VStack(spacing: 30) {
+                Text("Sveiks iPhone lietotāj!")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
+
+                NavigationLink(destination: SecondView()) {
+                    Text("Ej uz To-Do listu ➡️")
+                        .font(.title2)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+            }
+            .padding()
+            .navigationTitle("Sākums")
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
