005053b3e78c5f7bc775ce7f6a04f86a14c38c11
Groovy <linisr65@gmail.com>
2025-09-11 15:37:37 -0400

    Initial Commit

---
+++ xCareTests/xCareTests.swift
@@ -0,0 +1,16 @@
+//
+//  xCareTests.swift
+//  xCareTests
+//
+//  Created by Linards Rupmejs on 11/09/2025.
+//
+
+import Testing
+
+struct xCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
